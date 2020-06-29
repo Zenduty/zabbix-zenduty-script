01575b0b16f6360b6769f44024e7fc37b715cf0a
Ankur Rawal <ankurrawalnitk@gmail.com>
2020-06-29 20:43:52 +0530

    Update zabbix_zenduty_integration.sh
    Handling extra spaces with integration key

--- zabbix_zenduty_integration.sh
+++ zabbix_zenduty_integration.sh
@@ -1,3 +1,3 @@
-
 data="$2"
-curl -d "$data" -H "Content-Type: application/json" -X POST https://www.zenduty.com/api/integration/zabbix/$1/
+key=`echo $1 | sed 's/ *$//g'`
+curl -d "$data" -H "Content-Type: application/json" -X POST https://www.zenduty.com/api/integration/zabbix/$key/
